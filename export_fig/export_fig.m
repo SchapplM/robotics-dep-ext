0986f3172c1a93cff383d72738c12d802bf34a9c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-09-08 19:39:24 +0100

    [export_fig] Promo deaktiviert

--- export_fig/export_fig.m
+++ export_fig/export_fig.m
@@ -312,19 +312,6 @@ function [imageData, alpha] = export_fig(varargin) %#ok<*STRCL1>
     drawnow;
     pause(0.05);  % this solves timing issues with Java Swing's EDT (http://undocumentedmatlab.com/blog/solving-a-matlab-hang-problem)
 
-    % Display promo (just once!)
-    persistent promo
-    if isempty(promo) && ~isdeployed
-        website = 'https://UndocumentedMatlab.com';
-        link = ['<a href="' website];
-        msg = 'If you need expert assistance with Matlab, please consider my professional consulting/training services';
-        msg = [msg ' (' website ')'];
-        msg = regexprep(msg,website,[link '">$0</a>']);
-        msg = regexprep(msg,{'consulting','training'},[link '/$0">$0</a>']);
-        %warning('export_fig:promo',msg);
-        disp(['[' 8 msg ']' 8]);
-        promo = true;
-    end
 
     % Parse the input arguments
     fig = get(0, 'CurrentFigure');
